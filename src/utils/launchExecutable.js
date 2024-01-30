9cd5dd9047ebcfd0c99e0f9e46ff1237841fa401
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-01-29 22:10:05 -0500

    seems like unzipping is corrupting data somehow???

--- src/utils/launchExecutable.js
+++ src/utils/launchExecutable.js
@@ -1,14 +1,13 @@
-const {dirname} = require("path");
 const { spawn } = require('child_process');
 const path = require("path");
 const launchEmitter = new (require('events'))();
 const releasesPath = path.join(__dirname, '..', '..', 'cache', 'releases');
 
 function launchExecutable(executableName) {
-    const child = spawn(executableName, {
+    const child = spawn(`./${executableName}`, {
         detached: true,
         stdio: 'ignore',
-        cwd: dirname(path.join(releasesPath, executableName)),
+        cwd: path.join(releasesPath, executableName),
     });
 
     child.stdout.on('data', (data) => {
