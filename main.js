88c6416ed2c8acdd23e5ef5939eab4b40b64eb66
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-01-30 10:53:00 -0500

    specify node-pty beta

--- main.js
+++ main.js
@@ -35,6 +35,7 @@ function createWindow() {
             nodeIntegration: true,
             contextIsolation: false,
             enableRemoteModule: true,
+            nodeIntegrationInWorker: true,
         }
     })
 
